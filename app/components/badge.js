c49413fd166a68c3bccc83e18eb6490d995dd32e
Dmitry Demyankov <dmitry.demyankov@gmail.com>
2015-04-26 11:41:17 +0200

    09. React Native: Reusable components with required propType

---
+++ app/components/badge.js
@@ -0,0 +1,54 @@
+var React = require('react-native');
+
+var {
+  Text,
+  View,
+  Image,
+  StyleSheet
+} = React;
+
+var styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#48BBEC',
+    paddingBottom: 10
+  },
+  name: {
+    alignSelf: 'center',
+    fontSize: 21,
+    marginTop: 10,
+    marginBottom: 5,
+    color: 'white'
+  },
+  handle: {
+    alignSelf: 'center',
+    fontSize: 16,
+    color: 'white'
+  },
+  image: {
+    height: 125,
+    width: 125,
+    borderRadius: 65,
+    marginTop: 10,
+    alignSelf: 'center'
+  }
+});
+
+class Badge extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image 
+          source={{ uri: this.props.userInfo.avatar_url }}
+          style={styles.image} />
+        <Text style={styles.name}>{ this.props.userInfo.name }</Text>
+        <Text style={styles.handle}>{ this.props.userInfo.login }</Text>
+      </View>
+    );
+  }
+};
+
+Badge.propTypes = {
+  userInfo: React.PropTypes.object.isRequired
+};
+
+module.exports = Badge;
